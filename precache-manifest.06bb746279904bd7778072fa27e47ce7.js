343ce62d542060340f4f2d07055d33384bfbfec7
Krystian O <kr.olechowski@gmail.com>
2019-07-16 14:46:29 +0200

    Updates

---
+++ precache-manifest.06bb746279904bd7778072fa27e47ce7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3584dc7bd3c31dd0370d3979621799",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "c85ba3e6f2424bd95977",
+    "url": "/2048/static/css/main.b15c5b82.chunk.css"
+  },
+  {
+    "revision": "0fd71695187aa452ddef",
+    "url": "/2048/static/js/2.2f96a5fa.chunk.js"
+  },
+  {
+    "revision": "c85ba3e6f2424bd95977",
+    "url": "/2048/static/js/main.655454b9.chunk.js"
+  },
+  {
+    "revision": "e387bff5e0b18c0491fd",
+    "url": "/2048/static/js/runtime~main.f72a3c6a.js"
+  }
+]);
